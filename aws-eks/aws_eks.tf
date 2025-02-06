ec8e0313a508cf94347d8c8be742ff12668a134f
skwokie <skwok41@gmail.com>
2025-02-05 20:29:16 -0800

    Update number of nodes

--- aws-eks/aws_eks.tf
+++ aws-eks/aws_eks.tf
@@ -29,7 +29,7 @@ module "eks" {
 
       min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 1
 
       pre_bootstrap_user_data = <<-EOT
       echo 'foo bar'
@@ -47,7 +47,7 @@ module "eks" {
 
       min_size     = 1
       max_size     = 2
-      desired_size = 1
+      desired_size = 0
 
       pre_bootstrap_user_data = <<-EOT
       echo 'foo bar'
